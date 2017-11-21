916da7ff92be212338b4cda6662281d0db610508
MAEL <mael.petit@wanadoo.fr>
2017-11-21 14:30:39 +0100

    jhipster

---
+++ petitpaget/src/main/java/fr/istic/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package fr.istic.config.audit;
---
+++ petitpaget/src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,51 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import {
+    VideoGenSharedLibsModule,
+    VideoGenSharedCommonModule,
+    CSRFService,
+    AuthServerProvider,
+    AccountService,
+    UserService,
+    StateStorageService,
+    LoginService,
+    LoginModalService,
+    Principal,
+    JhiTrackerService,
+    HasAnyAuthorityDirective,
+    JhiLoginModalComponent
+} from './';
+
+@NgModule({
+    imports: [
+        VideoGenSharedLibsModule,
+        VideoGenSharedCommonModule
+    ],
+    declarations: [
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective
+    ],
+    providers: [
+        LoginService,
+        LoginModalService,
+        AccountService,
+        StateStorageService,
+        Principal,
+        CSRFService,
+        JhiTrackerService,
+        AuthServerProvider,
+        UserService,
+        DatePipe
+    ],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [
+        VideoGenSharedCommonModule,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective,
+        DatePipe
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class VideoGenSharedModule {}
