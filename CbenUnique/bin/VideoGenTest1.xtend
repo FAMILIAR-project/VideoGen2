8119e51eefedcf5da329d9926abd25e74de2286f
chakib <benkebir.chakib@gmail.com>
2018-01-14 22:17:37 +0100

    second

--- CbenUnique/bin/VideoGenTest1.xtend
+++ CbenUnique/bin/VideoGenTest1.xtend
@@ -1,165 +1,191 @@
+import java.io.PrintWriter
 import java.util.ArrayList
-import java.util.List
+import java.util.Random
 import org.eclipse.emf.common.util.URI
 import org.junit.Test
 import org.xtext.example.mydsl.videoGen.AlternativesMedia
 import org.xtext.example.mydsl.videoGen.MandatoryMedia
-import org.xtext.example.mydsl.videoGen.MediaDescription
 import org.xtext.example.mydsl.videoGen.OptionalMedia
 import org.xtext.example.mydsl.videoGen.VideoDescription
 
 import static org.junit.Assert.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class VideoGenTest1 {
-	val playlist = <VideoDescription>newArrayList
-	val descriptions = newArrayList()
-	String Playlist
-	//val listPlaylist= newArrayList(Playlist)
-	var List<String> msg = new ArrayList
-	
-	 
-	int alt=0
-	int opt=0
-	int poss=0
-	val videoGen = new VideoGenHelper().loadVideoGenerator(URI.createURI("example3.videogen"))
-	
+	int poss = 0
+	int alt = 0
 	int i
-	
-	String saut
-	
+	val random = new Random()
+	ArrayList<VideoDescription> playlist
+	ArrayList<ArrayList<VideoDescription>> playlists
+	String playmsg = ""
+	int totalvideos = 0
+	int opt = 0
+	var writer = new PrintWriter("videos/playlist.txt", "UTF-8")
+	ArrayList<VideoDescription> longplaylist = new ArrayList<VideoDescription>
+	val videoGen = new VideoGenHelper().loadVideoGenerator(URI.createURI("example3.videogen"))
+	String command
+	String startcommand
+	Process process
+	BufferedReader stdInput
+	String s
+	BufferedReader stdError
+
 	@Test
 	def void testLoadModel() {
-		
-		
+		startcommand = "ffmpeg -i "
+		command = " 2>&1 | grep \""
+		command = command + "Duration\""
+		command = command + "| cut -d ' ' -f 4 | sed s/,// | sed 's@\\" + "..*@@g' | awk '{ split($1, A, \""
+		command = command + ":\""
+		command = command + "); split(A[3], B, \""
+		command = command + ".\""
+		command = command + "); print 3600*A[1] + 60*A[2] + B[1] }'"
 		assertNotNull(videoGen)
-		println(videoGen.information.authorName)		
+		println(videoGen.information.authorName)
+		// println(startcommand+command)
 		// and then visit the model
 		// eg access video sequences: videoGen.videoseqs
-		//getPossiility()
-		
-		videoGen.medias.forEach[m |
-			
-			if (m instanceof MandatoryMedia)
-			{
-				
-				if(m.description instanceof VideoDescription)
-				{
-					//executeVLC((m.description as VideoDescription))
-					//playlist.add(m.description)
-				}	
-			}
-			if (m instanceof OptionalMedia)
-			{
-				//random
-				//idem
-				
-			}
-			if (m instanceof AlternativesMedia)
-			{
-				//random with choice
-				//idem
-			}
-			
-			
-			
-			
-			
-			
-		]
-		getSequences()
-		
-		
-	}
-	def getSequences(){
-					saut="\n";
-		videoGen.medias.forEach[m |
-			
-			if (m instanceof MandatoryMedia)
-			{
-				for(i=0;i<3;i++){
-					 
-					msg.add(m.description.location)
-					//Playlist=msg.get(i)
-					Playlist="2"
-					println(Playlist+"xxxxxx")
-					//Playlist=Playlist+m.description.toString
-					
-					//msg.set(i, Playlist)
-					
-					
-				}
-				
-			}
-			if (m instanceof OptionalMedia)
-			{
-				//random
-				//idem
-				
-			}
-			if (m instanceof AlternativesMedia)
-			{
-				//random with choice
-				//idem
-			}
-			
-			
-			
-			
-			
-			
-		]
-		println(msg)
-		
-		
-	}
-	def copmpilePlaylist(ArrayList<MediaDescription> l){
-		//FileHandler f = "playlist.txt"
-		for(description : descriptions){
-			//l.add(description)
-			
+		// getPossiility()
+		totalvideos = getPossility()
+		println("total :" + totalvideos)
+
+		playlists = new ArrayList<ArrayList<VideoDescription>>
+		for (i = 0; i < totalvideos; i++) {
+
+			playlists.add(new ArrayList<VideoDescription>)
+
+		// println("1")		
 		}
-		
-		}	
-	def int getPossiility(){
-		
-		val videoGen = new VideoGenHelper().loadVideoGenerator(URI.createURI("example1.videogen"))
-		assertNotNull(videoGen)
-		println(videoGen.information.authorName)		
-		//listPlaylist.add();
-		videoGen.medias.forEach[m |
-			
-			
-			if (m instanceof OptionalMedia)
-			{
-				//random
-				//idem
-				opt=opt+2
+		videoGen.medias.forEach [ m |
+
+			if (m instanceof MandatoryMedia) {
+				for (i = 0; i < totalvideos; i++) {
+
+					playlists.get(i).add(m.description as VideoDescription)
+
+				}
+				playmsg = playmsg + m.description.location + "\n"
+				longplaylist.add(m.description as VideoDescription)
+
+			// medias.add (new Media(m.description.location,1 ))
 			}
-			if (m instanceof AlternativesMedia)
-			{
-				//random with choice
-				//idem
-				
-				alt=alt+m.medias.size()
-				//println("alternative "+m.medias.size)		
-				
-			}	
+			if (m instanceof OptionalMedia) {
+				// medias.add (new Media(m.description.location,2 ))
+				for (i = 0; i < totalvideos / 2; i++) {
+
+					playlists.get(i).add(m.description as VideoDescription)
+
+				}
+				longplaylist.add(m.description as VideoDescription)
+				playmsg = playmsg + m.description.location + "\n"
+
+			}
+			if (m instanceof AlternativesMedia) {
+				// medtmp= new Media ("",3) 
+				alt = m.medias.size
+				m.medias.forEach [ alter |
+					// altmedia.medias.add (new Media (alter.location.toString  )); 
+					// medtmp.altmedias.add (new Media(alter.location,1 ))
+					for (i = poss; i < totalvideos; i = i + alt) {
+
+						playlists.get(i).add(alter as VideoDescription)
+						playmsg = playmsg + alter.location + "\n"
+
+					}
+					// println("Here is the standard output of the command:\n");
+					s = startcommand+alter.location+command
+					println(s)
+					process = Runtime.runtime.exec(s)
+					// startcommand+alter.location+command
+					// println(startcommand+alter.location+command)
+					stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()))
+					stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()))
+					process.getOutputStream().toString
+					while ((s = stdInput.readLine()) !== null) {
+						println("//"+s);
+					}
+					while ((s = stdError.readLine()) !== null) {
+						System.out.println("Err:"+s);
+					}
+					poss++
+				]
+				poss = 0
+			}
 		]
-		if(opt==0) opt++
-		if(alt==0) alt++
-		poss=opt*alt
-		//println(poss)		
-		return poss
-		
+
+		/*
+		 * println("Debut*************************\n")
+		 * println("playlists   :\n")
+		 * playlists.forEach[list|
+		 * 				println("element:\n")	  
+		 * 				list.forEach[element|
+		 * 					
+		 * 				print(element.videoid+";")	  
+		 * 					
+		 * 					
+		 * 				]	
+		 * 							 println("\n")	  
+		 * ]
+		 * 			 println("\n")	  
+		 * 
+		 * 			println("Fin*************************\n")	  
+		 */
+		// println(random.nextInt(totalvideos))
+		playlist = playlists.get(random.nextInt(totalvideos))
+	// createffFile()
+	// executeVLC()
+	// CsvParser
 	}
-	def compileffFile(){
-		
-		
-		
+
+	def int getPossility() {
+		videoGen.medias.forEach [ m |
+
+			if (m instanceof OptionalMedia) {
+				// random
+				// idem
+				opt = opt + 2
+			}
+			if (m instanceof AlternativesMedia) {
+				// random with choice
+				// idem
+				if (alt == 0) {
+					alt = alt + m.medias.size()
+				} else {
+					alt = alt * m.medias.size()
+				}
+
+			// println("alternative "+m.medias.size)		
+			}
+		]
+		if(opt == 0) opt++
+		if(alt == 0) alt++
+
+		return opt * alt
+
 	}
-	def executeVLC(VideoDescription d){
-		
-		var p =Runtime.runtime.exec("vlc "+d.location)
+
+	def createffFile() {
+		writer.println("#bref file");
+		playlist.forEach [ video |
+			writer.println("file '" + video.location + "'");
+
+		]
+		writer.close();
+
+		Runtime.runtime.exec("ffmpeg -f concat 0 -i ./playlist.txt -c copy ./bref.mp4")
+	}
+
+	def executeVLC() {
+		// file:///home/chak/Documents/IDM/projet/VideoGen2/CbenUnique/bref.mp4
+		var p = Runtime.runtime.exec("vlc  videos/bref.mp4")
 		p.waitFor
+	}
+
+	def longest() {
+		var p = Runtime.runtime.exec("vlc  videos/long.mp4")
+		p.waitFor
+		
 	}
 }
--- CbenUnique/xtend-gen/VideoGenTest1.java
+++ CbenUnique/xtend-gen/VideoGenTest1.java
@@ -1,9 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 import java.util.function.Consumer;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.xtext.xbase.lib.CollectionLiterals;
 import org.eclipse.xtext.xbase.lib.Exceptions;
+import org.eclipse.xtext.xbase.lib.Functions.Function0;
 import org.eclipse.xtext.xbase.lib.InputOutput;
 import org.junit.Assert;
 import org.junit.Test;
@@ -17,112 +21,209 @@ import org.xtext.example.mydsl.videoGen.VideoGeneratorModel;
 
 @SuppressWarnings("all")
 public class VideoGenTest1 {
-  private final ArrayList<VideoDescription> playlist = CollectionLiterals.<VideoDescription>newArrayList();
-  
-  private final ArrayList<Object> descriptions = CollectionLiterals.<Object>newArrayList();
-  
-  private String Playlist;
-  
-  private List<String> msg = new ArrayList<String>();
+  private int poss = 0;
   
   private int alt = 0;
   
+  private int i;
+  
+  private final Random random = new Random();
+  
+  private ArrayList<VideoDescription> playlist;
+  
+  private ArrayList<ArrayList<VideoDescription>> playlists;
+  
+  private String playmsg = "";
+  
+  private int totalvideos = 0;
+  
   private int opt = 0;
   
-  private int poss = 0;
+  private PrintWriter writer = new Function0<PrintWriter>() {
+    public PrintWriter apply() {
+      try {
+        PrintWriter _printWriter = new PrintWriter("videos/playlist.txt", "UTF-8");
+        return _printWriter;
+      } catch (Throwable _e) {
+        throw Exceptions.sneakyThrow(_e);
+      }
+    }
+  }.apply();
+  
+  private ArrayList<VideoDescription> longplaylist = new ArrayList<VideoDescription>();
   
   private final VideoGeneratorModel videoGen = new VideoGenHelper().loadVideoGenerator(URI.createURI("example3.videogen"));
   
-  private int i;
+  private String command;
   
-  private String saut;
+  private String startcommand;
+  
+  private Process process;
+  
+  private BufferedReader stdInput;
+  
+  private String s;
+  
+  private BufferedReader stdError;
   
   @Test
   public void testLoadModel() {
+    this.startcommand = "ffmpeg -i ";
+    this.command = " 2>&1 | grep \"";
+    this.command = (this.command + "Duration\"");
+    this.command = ((this.command + "| cut -d \' \' -f 4 | sed s/,// | sed \'s@\\") + "..*@@g\' | awk \'{ split($1, A, \"");
+    this.command = (this.command + ":\"");
+    this.command = (this.command + "); split(A[3], B, \"");
+    this.command = (this.command + ".\"");
+    this.command = (this.command + "); print 3600*A[1] + 60*A[2] + B[1] }\'");
     Assert.assertNotNull(this.videoGen);
     InputOutput.<String>println(this.videoGen.getInformation().getAuthorName());
+    this.totalvideos = this.getPossility();
+    InputOutput.<String>println(("total :" + Integer.valueOf(this.totalvideos)));
+    ArrayList<ArrayList<VideoDescription>> _arrayList = new ArrayList<ArrayList<VideoDescription>>();
+    this.playlists = _arrayList;
+    for (this.i = 0; (this.i < this.totalvideos); this.i++) {
+      ArrayList<VideoDescription> _arrayList_1 = new ArrayList<VideoDescription>();
+      this.playlists.add(_arrayList_1);
+    }
     final Consumer<Media> _function = (Media m) -> {
       if ((m instanceof MandatoryMedia)) {
-        MediaDescription _description = ((MandatoryMedia)m).getDescription();
-        if ((_description instanceof VideoDescription)) {
+        for (this.i = 0; (this.i < this.totalvideos); this.i++) {
+          MediaDescription _description = ((MandatoryMedia)m).getDescription();
+          this.playlists.get(this.i).add(((VideoDescription) _description));
         }
+        String _location = ((MandatoryMedia)m).getDescription().getLocation();
+        String _plus = (this.playmsg + _location);
+        String _plus_1 = (_plus + "\n");
+        this.playmsg = _plus_1;
+        MediaDescription _description = ((MandatoryMedia)m).getDescription();
+        this.longplaylist.add(((VideoDescription) _description));
       }
       if ((m instanceof OptionalMedia)) {
+        for (this.i = 0; (this.i < (this.totalvideos / 2)); this.i++) {
+          MediaDescription _description_1 = ((OptionalMedia)m).getDescription();
+          this.playlists.get(this.i).add(((VideoDescription) _description_1));
+        }
+        MediaDescription _description_1 = ((OptionalMedia)m).getDescription();
+        this.longplaylist.add(((VideoDescription) _description_1));
+        String _location_1 = ((OptionalMedia)m).getDescription().getLocation();
+        String _plus_2 = (this.playmsg + _location_1);
+        String _plus_3 = (_plus_2 + "\n");
+        this.playmsg = _plus_3;
       }
       if ((m instanceof AlternativesMedia)) {
+        this.alt = ((AlternativesMedia)m).getMedias().size();
+        final Consumer<MediaDescription> _function_1 = (MediaDescription alter) -> {
+          try {
+            for (this.i = this.poss; (this.i < this.totalvideos); this.i = (this.i + this.alt)) {
+              {
+                this.playlists.get(this.i).add(((VideoDescription) alter));
+                String _location_2 = alter.getLocation();
+                String _plus_4 = (this.playmsg + _location_2);
+                String _plus_5 = (_plus_4 + "\n");
+                this.playmsg = _plus_5;
+              }
+            }
+            String _location_2 = alter.getLocation();
+            String _plus_4 = (this.startcommand + _location_2);
+            String _plus_5 = (_plus_4 + this.command);
+            this.s = _plus_5;
+            InputOutput.<String>println(this.s);
+            this.process = Runtime.getRuntime().exec(this.s);
+            InputStream _inputStream = this.process.getInputStream();
+            InputStreamReader _inputStreamReader = new InputStreamReader(_inputStream);
+            BufferedReader _bufferedReader = new BufferedReader(_inputStreamReader);
+            this.stdInput = _bufferedReader;
+            InputStream _errorStream = this.process.getErrorStream();
+            InputStreamReader _inputStreamReader_1 = new InputStreamReader(_errorStream);
+            BufferedReader _bufferedReader_1 = new BufferedReader(_inputStreamReader_1);
+            this.stdError = _bufferedReader_1;
+            this.process.getOutputStream().toString();
+            while (((this.s = this.stdInput.readLine()) != null)) {
+              InputOutput.<String>println(("//" + this.s));
+            }
+            while (((this.s = this.stdError.readLine()) != null)) {
+              System.out.println(("Err:" + this.s));
+            }
+            this.poss++;
+          } catch (Throwable _e) {
+            throw Exceptions.sneakyThrow(_e);
+          }
+        };
+        ((AlternativesMedia)m).getMedias().forEach(_function_1);
+        this.poss = 0;
       }
     };
     this.videoGen.getMedias().forEach(_function);
-    this.getSequences();
+    this.playlist = this.playlists.get(this.random.nextInt(this.totalvideos));
   }
   
-  public List<String> getSequences() {
-    List<String> _xblockexpression = null;
-    {
-      this.saut = "\n";
-      final Consumer<Media> _function = (Media m) -> {
-        if ((m instanceof MandatoryMedia)) {
-          for (this.i = 0; (this.i < 3); this.i++) {
-            {
-              this.msg.add(((MandatoryMedia)m).getDescription().getLocation());
-              this.Playlist = "2";
-              InputOutput.<String>println((this.Playlist + "xxxxxx"));
-            }
-          }
-        }
-        if ((m instanceof OptionalMedia)) {
-        }
-        if ((m instanceof AlternativesMedia)) {
-        }
-      };
-      this.videoGen.getMedias().forEach(_function);
-      _xblockexpression = InputOutput.<List<String>>println(this.msg);
-    }
-    return _xblockexpression;
-  }
-  
-  public void copmpilePlaylist(final ArrayList<MediaDescription> l) {
-    for (final Object description : this.descriptions) {
-    }
-  }
-  
-  public int getPossiility() {
-    final VideoGeneratorModel videoGen = new VideoGenHelper().loadVideoGenerator(URI.createURI("example1.videogen"));
-    Assert.assertNotNull(videoGen);
-    InputOutput.<String>println(videoGen.getInformation().getAuthorName());
+  public int getPossility() {
     final Consumer<Media> _function = (Media m) -> {
       if ((m instanceof OptionalMedia)) {
         this.opt = (this.opt + 2);
       }
       if ((m instanceof AlternativesMedia)) {
-        int _size = ((AlternativesMedia)m).getMedias().size();
-        int _plus = (this.alt + _size);
-        this.alt = _plus;
+        if ((this.alt == 0)) {
+          int _size = ((AlternativesMedia)m).getMedias().size();
+          int _plus = (this.alt + _size);
+          this.alt = _plus;
+        } else {
+          int _size_1 = ((AlternativesMedia)m).getMedias().size();
+          int _multiply = (this.alt * _size_1);
+          this.alt = _multiply;
+        }
       }
     };
-    videoGen.getMedias().forEach(_function);
+    this.videoGen.getMedias().forEach(_function);
     if ((this.opt == 0)) {
       this.opt++;
     }
     if ((this.alt == 0)) {
       this.alt++;
     }
-    this.poss = (this.opt * this.alt);
-    return this.poss;
+    return (this.opt * this.alt);
   }
   
-  public Object compileffFile() {
-    return null;
+  public Process createffFile() {
+    try {
+      Process _xblockexpression = null;
+      {
+        this.writer.println("#bref file");
+        final Consumer<VideoDescription> _function = (VideoDescription video) -> {
+          String _location = video.getLocation();
+          String _plus = ("file \'" + _location);
+          String _plus_1 = (_plus + "\'");
+          this.writer.println(_plus_1);
+        };
+        this.playlist.forEach(_function);
+        this.writer.close();
+        _xblockexpression = Runtime.getRuntime().exec("ffmpeg -f concat 0 -i ./playlist.txt -c copy ./bref.mp4");
+      }
+      return _xblockexpression;
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
   }
   
-  public int executeVLC(final VideoDescription d) {
+  public int executeVLC() {
     try {
       int _xblockexpression = (int) 0;
       {
-        Runtime _runtime = Runtime.getRuntime();
-        String _location = d.getLocation();
-        String _plus = ("vlc " + _location);
-        Process p = _runtime.exec(_plus);
+        Process p = Runtime.getRuntime().exec("vlc  videos/bref.mp4");
+        _xblockexpression = p.waitFor();
+      }
+      return _xblockexpression;
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+  
+  public int longest() {
+    try {
+      int _xblockexpression = (int) 0;
+      {
+        Process p = Runtime.getRuntime().exec("vlc  videos/long.mp4");
         _xblockexpression = p.waitFor();
       }
       return _xblockexpression;
